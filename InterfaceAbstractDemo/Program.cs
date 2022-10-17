6f617bd6baf40a0628b9f14af0effb7cac671ee5
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-17 17:15:10 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id = 1,
+                FirstName = "Oğuz Kağan",
+                LastName = "ER",
+                DateofBirth = new DateTime(1994,3, 27),
+                NationalityId = "22222222222"
+
+            };
+
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(customer1);
+
+            Console.ReadLine();
+
+        }
+    }
+}
